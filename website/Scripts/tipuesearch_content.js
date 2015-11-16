00c310a9d89ac777b4f9c19b8ecc63315210230f
mxg0633 <mgusmano@luc.edu>
2015-11-15 20:27:13 -0600

    working search bar

---
+++ website/Scripts/tipuesearch_content.js
@@ -0,0 +1,20 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Tipue", "text": "", "tags": "jQuery HTML5 CSS", "url": "http://www.tipue.com"},
+     {"title": "About Tipue", "text": "Tipue is a small web development studio based in North London. We've been around for well over a decade. We design innovative add-ins, plugins, code and features with heavy-duty Perl, MySQL and jQuery.", "tags": "", "url": "http://www.tipue.com/about"},     
+     {"title": "The Tipue blog - The complete guide to centering a div", "text": "Every new developer inevitably finds that centering a div isn't as obvious as you'd expect. Centering what's inside a div is easy enough by giving the text-align property a value of center, but then things tend to get a bit sticky. When you get to centering a div vertically, you can end up in a world of CSS hurt.", "tags": "HTML", "url": "http://www.tipue.com/blog/center-a-div"},
+     {"title": "The Tipue blog - Native HTML5 autocomplete with input list", "text": "This article shows how with the HTML5 input list attribute and datalist element you can easily set up an input box with a custom autocomplete without jQuery, JavaScript or similar.", "tags": "", "url": "http://www.tipue.com/blog/input-list"},
+     {"title": "The Tipue blog - The really simple guide to z-index", "text": "The CSS z-index property often trips up new and even experienced developers. The aim of this article is to boil down a somewhat-complex specification to 3 major points, which should ease most z-index pain.", "tags": "", "url": "http://www.tipue.com/z-index"},
+
+     {"title": "Asia", "text": "An occasional blog covering CSS, web development, etc.", "tags": "Asia", "url": "Asia.html"},
+     {"title": "South America", "text": "An occasional blog covering CSS, web development, etc.", "tags": "South America", "url": "southamerica.html"},
+     {"title": "North America", "text": "An occasional blog covering CSS, web development, etc.", "tags": "North America", "url": "northamerica.html"},
+     {"title": "Central America", "text": "An occasional blog covering CSS, web development, etc.", "tags": "Central America", "url": "centralamerica.html"},
+     {"title": "Europe", "text": "An occasional blog covering CSS, web development, etc.", "tags": "Europe", "url": "europe.html"},
+     {"title": "Africa", "text": "An occasional blog covering CSS, web development, etc.", "tags": "Africa", "url": "africa.html"},
+     {"title": "Asia", "text": "An occasional blog covering CSS, web development, etc.", "tags": "Asia", "url": "Asia.html"},
+     {"title": "Asia", "text": "An occasional blog covering CSS, web development, etc.", "tags": "Asia", "url": "Asia.html"}
+]};
+
+
+
